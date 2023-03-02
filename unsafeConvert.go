1f4ce3f4a206ef5e4ac0f07f87a4659cd5a5940d
Zero <54469706+3JoB@users.noreply.github.com>
2023-03-02 21:50:14 +0800

    change

--- unsafeConvert.go
+++ unsafeConvert.go
@@ -1,8 +1,12 @@
 //go:build go1.20
 // +build go1.20
+
 package unsafeConvert
 
-import "unsafe"
+import (
+	"strconv"
+	"unsafe"
+)
 
 func Bytes(v string) []byte {
 	return unsafe.Slice(unsafe.StringData(v), len(v))
@@ -10,4 +14,22 @@ func Bytes(v string) []byte {
 
 func String(v []byte) string {
 	return unsafe.String(&v[0], len(v))
+}
+
+func IntToString(v int) string {
+	return strconv.Itoa(v)
+}
+
+func Int64ToString(v int64) string {
+	return strconv.FormatInt(v, 10)
+}
+
+func StringToInt64(v string) int64 {
+	i, _ := strconv.ParseInt(v, 10, 64)
+	return i
+}
+
+func StringToInt(v string) int {
+	i, _ := strconv.Atoi(v)
+	return i
 }
